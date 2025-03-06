7ef66866f1e7bf4dd55502d925974f01475ed22d
unknown <dima.sufranovich@gmail.com>
2025-03-06 22:05:55 +0300

    first commit

---
+++ round_2.js
@@ -0,0 +1,39 @@
+// Что женщины чаще всего делают с подаренными цветами?
+const answers = [
+    { text: "Выкладывают в instagram/Фотографируют.", percentage: 40 },
+    { text: "Ставят в вазу.", percentage: 25 },
+    { text: "Раздают родственникам.", percentage: 20 },
+    { text: "Удивляются, если цветов нет.", percentage: 15 },
+    { text: "Оставляют в офисе.", percentage: 10 },
+    { text: "Дарят любовнику.", percentage: 5 }
+  ]
+
+// Какие подарки считают самыми бесполезными на 8 марта?
+const answers = [
+    { text: "Сковородки.", percentage: 40 },
+    { text: "Простые открытки.", percentage: 25 },
+    { text: "Книги по самообразованию.", percentage: 20 },
+    { text: "Приборы для дома.", percentage: 15 },
+    { text: "Товары для уборки.", percentage: 10 },
+    { text: "Офисные канцелярии.", percentage: 5 }
+  ]
+
+// Что мужчины чаще всего покупают в последний момент?
+const answers = [
+    { text: "Цветы.", percentage: 40 },
+    { text: "Конфеты.", percentage: 25 },
+    { text: "Парфюм.", percentage: 20 },
+    { text: "Косметику.", percentage: 15 },
+    { text: "Ювелирные изделия.", percentage: 10 },
+    { text: "Подарочные сертификаты.", percentage: 5 }
+  ]
+
+// Что мужчины делают, если подарок не понравился? (Запасной вопрос)
+const answers = [
+    { text: "Молчат.", percentage: 40 },
+    { text: "Пытаются исправить ситуацию.", percentage: 25 },
+    { text: "Объясняют свою мысль.", percentage: 20 },
+    { text: "Заменяют подарок.", percentage: 15 },
+    { text: "Возвращаются в магазин.", percentage: 10 },
+    { text: "Спрашивают совета.", percentage: 5 }
+  ]
